64729ff0cafbe538e8d5b27a2a23341baf82e67a
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-04-24 15:06:40 +0900

    test

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,3 +1,17 @@
 float4 main( float4 pos : POSITION ) : SV_POSITION{
 	return pos;
 }
+
+struct VertexShaderOutput {
+	float32_t4 position : SV_POSITION;
+};
+
+struct VertexShaderInput {
+	float32_t4 position : POSITION0;
+};
+
+VetexShaderOutput main(VertexShaderInput input) {
+	VertexShaderOutout output;
+	output.position = input.position;
+	return output;
+}
