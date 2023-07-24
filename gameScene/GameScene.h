db3fd63de9f110ca7089e559d3394ed5cc4b5b6e
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-07-24 15:24:24 +0900

    修正

--- gameScene/GameScene.h
+++ gameScene/GameScene.h
@@ -16,19 +16,10 @@ private:
 	CitrusJunosEngine* CJEngine_;
 	DirectXCommon* dxCommon_;
 
-	CreateTriangle* triangle_[2];
-	TriangleData triangleData_[2];
+	CreateTriangle* triangle_[3];
+	TriangleData triangleData_[3];
 	Transform transform_;
 	Matrix4x4 worldMatrix_;
-
-	CreateSprite* sprite_[2];
-	SpriteData spriteData_;
-	Transform spriteTransform_;
-	
-	CreateSphere* sphere_;
-	Transform sphereTransform_;
-	Vector4 sphereMaterial_;
-	Matrix4x4 sphereMatrix_;
 
 	Transform cameraTransform_;
 };
